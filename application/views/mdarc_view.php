1768cb8f07f335ad9ef9726a7c650caff9515192
prager <jkulisek.us@gmail.com>
2023-12-17 17:07:03 -0800

    Added icon

--- application/views/mdarc_view.php
+++ application/views/mdarc_view.php
@@ -4,6 +4,7 @@
     <title>MDARC Payments</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="shortcut icon" href="<?php echo base_url() ;?>assets/img/mdarc-icon.ico" type="image/x-icon" />
     <style type="text/css">
 
         .panel-title {
@@ -27,6 +28,9 @@
     </style>
 </head>
 <body>
+
+<!-- Modals: https://getbootstrap.com/docs/3.3/javascript/#modals -->
+
 <div class="container">
 	<div class="row">
 		<div class="col co-md-offset-3 text-center">
@@ -171,8 +175,10 @@
             &nbsp;
         </div>
     </div>
+
 </div>
 </body> 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 <script type="text/javascript">
 
